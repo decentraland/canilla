d1a99d26156d552d03db1bd9270659eb19323fee
Kyllian LE BORGNE ROEPRCH <me@hprivakos.net>
2021-05-23 11:05:19 +0200

    fix: add alias for goerli

--- webapp/.ci/index.ts
+++ webapp/.ci/index.ts
@@ -4,15 +4,36 @@ import { buildStatic } from 'dcl-ops-lib/buildStatic'
 import { getDomainAndSubdomain } from 'dcl-ops-lib/getDomainAndSubdomain'
 
 async function main() {
-  const faucetRopsten = buildStatic({
+  const faucet = buildStatic({
     domain: `faucet.decentraland.io`,
     additionalDomains: [`faucet-goerli.decentraland.io`],
     defaultPath: 'index.html',
   })
 
+  createAliasRecord(`faucet-goerli.decentraland.io`, { hostedZoneId: faucet.cloudfrontDistribution, domainName: faucet.cloudFrontDomain })
+
   return {
-    cloudfrontDistribution: faucetRopsten.cloudfrontDistribution,
-    bucketName: faucetRopsten.contentBucket,
+    cloudfrontDistribution: faucet.cloudfrontDistribution,
+    bucketName: faucet.contentBucket,
   }
 }
 export = main
+
+function createAliasRecord(targetDomain: string, distribution: { hostedZoneId: pulumi.Output<string>, domainName: pulumi.Output<string> }): aws.route53.Record {
+  const domainParts = getDomainAndSubdomain(targetDomain)
+  const hostedZoneId = aws.route53
+    .getZone({ name: domainParts.parentDomain }, { async: true })
+    .then((zone: { zoneId: string }) => zone.zoneId)
+  return new aws.route53.Record(targetDomain, {
+    name: domainParts.subdomain,
+    zoneId: hostedZoneId,
+    type: "A",
+    aliases: [
+      {
+        name: distribution.domainName,
+        zoneId: distribution.hostedZoneId,
+        evaluateTargetHealth: true,
+      },
+    ],
+  })
+}
